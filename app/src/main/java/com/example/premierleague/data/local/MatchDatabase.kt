7d132172a4c25bb645e9bb973420f12ecea49ada
DamirTsisthvaya <cichvaadamirgmail.com@mail.ru>
2025-07-17 23:02:53 +0300

    Update MatchDatabase.kt

--- app/src/main/java/com/example/premierleague/data/local/MatchDatabase.kt
+++ app/src/main/java/com/example/premierleague/data/local/MatchDatabase.kt
@@ -9,20 +9,4 @@ import com.example.premierleague.model.Match
 abstract class MatchDatabase : RoomDatabase() {
     abstract fun matchDao(): MatchDao
 
-    companion object {
-        @Volatile
-        private var INSTANCE: MatchDatabase? = null
-
-        fun getDatabase(context: Context): MatchDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    MatchDatabase::class.java,
-                    "match_database"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
